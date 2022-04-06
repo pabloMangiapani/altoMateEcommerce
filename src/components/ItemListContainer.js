85dc538b3dbb54d7599cc4cce3c15469fd25ef7a
pabloMangiapani <pmangiapani23@gmail.com>
2022-04-05 22:48:01 -0300

    desafio componentes dos

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,11 +1,52 @@
-const ItemListContainer = (props) => {
+// const ItemListContainer = (props) => {
+//     return (
+//         <>
+//         <div>
+//             <span>{props.greeting}</span>
+//         </div>
+//         </>
+//     );
+// }
+
+import { useEffect, useState } from "react"
+import { Button } from '@material-ui/core';
+import { AddCircleOutlined, RemoveCircleOutlined } from '@mui/icons-material';
+import { ItemCounterContainer, ItemCounter } from './styledComponents';
+
+
+
+
+const ItemListContainer = ({ stock = 20, initial = 1, onAdd} ) => {
+    const [count, setCount] = useState(0);
+
+    useEffect(() =>{
+        setCount(initial);
+    }, [] );
+
+    const addItem = () => {
+        if (count < stock) {
+            setCount(count + 1);
+        }
+    }
+
+    const removeItem = () => {
+        if (count > initial) {
+            setCount(count - 1);
+        }
+    }
+
     return (
-        <>
+        <ItemCounterContainer>
+        <Button variant="text" onClick={addItem}><AddCircleOutlined color="success" /></Button>
+        <ItemCounter>{count}</ItemCounter>
+        <Button variant="text" onClick={removeItem}><RemoveCircleOutlined color="secondary" /></Button>
+
         <div>
-            <span>{props.greeting}</span>
+             <Button variant="contained" color="secondary" onClick={() => onAdd(count)}>Add to Cart</Button>
         </div>
-        </>
-    );
+
+        </ItemCounterContainer>
+    )
 }
 
 export default ItemListContainer;
---
+++ src/components/styledComponents.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const ItemCounterContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin: 20px;
+    margin-top: 15px;
+`;
+
+const ItemCounter = styled.div`
+    font-size: 24px;
+    margin: 5px;
+`;
+
+export {
+    ItemCounterContainer, ItemCounter
+}
